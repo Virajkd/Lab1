eefa04e6844b0d2f3ce7e9310daaeac912136d71
Virajkd <150107031+Virajkd@users.noreply.github.com>
2024-07-28 19:51:12 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,39 +1,39 @@
-def matrix_creation(rows,columns):
-    for i in range (rows):
-        for j in range (columns):
-            mi[i][j]=int(input("enter at position ({i},{j})"))
-    return mi
-
+def matrix_creation(r,c):
+    a=[[0]*c for i in range (r)]
+    for i in range (r): 
+        for j in range (c):
+            print("Enter [",i,j,"] element")
+            a[i][j]=int(input())
+    return a 
 
 def matrix_check(m1,m2):
-    r_m1,r_m2,c_m1,c_m2=0
-    for i in m1:
-        r_m1+=1
-        for j in i:
-            c_m1+=1
-    for i in m2:
-        r_m2+=1
-        for j in i:
-            c_m2+=1
-    if (r_m1==c_m2) and (r_m2==c_m1):
+    if ( len(m2)==len(m1[0])):
         return True
     return False
 
+
 def matrix_multiplication(m1,m2):
-    if (matrix_check()):
-        for i in m1:
-            for j in i:
-                sum=m1[i][j]*m2[j][i]
-    return sum
+    sum=0
+    if (matrix_check(m1,m2)):
+        for i in range (len(m1)):
+            for j in range (len(m1[i])):
+                sum+=m1[i][j]*m2[j][i]
+    
+    else:
+        print("Matrix cant be multiplied")
+        return
+        
+    print("Sum=",sum)
 
-
-print("Matrix 1(m1)")
+print("Matrix 1")
 rows,columns=eval(input("enter the number of rows n columns"))
 m1=matrix_creation(rows,columns)
-print("Matrix 2(m2)")
+print(m1)
+print("Matrix 2")
 rows,columns=eval(input("enter the number of rows n columns"))
 m2=matrix_creation(rows,columns)
-sum=matrix_multiplication(m1,m2)
-print("Sum=",sum)
+print(m2)
+matrix_multiplication(m1,m2)
+
 
     
